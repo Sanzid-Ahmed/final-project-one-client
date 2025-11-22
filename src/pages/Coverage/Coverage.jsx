8e565682240f95818ed671d4919efb8228d9020b
Sanzid Ahmed <sanzidahmed313@gmail.com>
2025-11-22 19:45:33 +0600

    added map successfully

--- src/pages/Coverage/Coverage.jsx
+++ src/pages/Coverage/Coverage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { useLoaderData } from "react-router";
@@ -6,13 +6,25 @@ import { useLoaderData } from "react-router";
 const Coverage = () => {
   const position = [23.6858, 90.3563];
   const serviceCenters = useLoaderData();
+  const mapRef = useRef(null);
   console.log(serviceCenters);
+
+    const handleSearch = e =>{
+        e.preventDefault();
+
+        const location = e.target.location.value;
+        const district = serviceCenters.find(c => c.district.toLowerCase().includes(location.toLowerCase()));
+        if(district){
+            const coord = [district.latitude, district.longitude];
+            mapRef.current.flyTo(coord, 14)
+        }
+    } 
 
   return (
     <div>
       <h2 className="text-5xl">We are available in 64 districts</h2>
       <div>
-        <form>
+        <form onSubmit={handleSearch}>
           <label className="input">
             <svg
               className="h-[1em] opacity-50"
@@ -30,9 +42,7 @@ const Coverage = () => {
                 <path d="m21 21-4.3-4.3"></path>
               </g>
             </svg>
-            <input type="search" className="grow" placeholder="Search" />
-            <kbd className="kbd kbd-sm">⌘</kbd>
-            <kbd className="kbd kbd-sm">K</kbd>
+            <input type="search" name="location" className="grow" placeholder="Search" />
           </label>
         </form>
       </div>
@@ -42,6 +52,7 @@ const Coverage = () => {
           zoom={8}
           scrollWheelZoom={false}
           className="h-[800px]"
+          ref={mapRef}
         >
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
