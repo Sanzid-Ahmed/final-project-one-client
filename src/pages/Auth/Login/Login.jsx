abe57045bc9e9362bc5f6ed3283d9b6278e98014
Sanzid Ahmed <sanzidahmed313@gmail.com>
2025-11-25 13:07:45 +0600

    added login option successfully

--- src/pages/Auth/Login/Login.jsx
+++ src/pages/Auth/Login/Login.jsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import React from "react";
+import { useForm } from "react-hook-form";
+import useAuth from "../../../hooks/useAuth";
 
 const Login = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+
+
+    const { register, handleSubmit, formState: {errors} } = useForm();
+    const { signInUser } = useAuth();
+    const handleLogin = (data) =>{
+        signInUser(data.email, data.password).then(result => {
+            console.log(result.user);
+        }).catch(error => {
+            console.log(error);
+        })
+    }
+
+  return (
+    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+      <form className="card-body" onSubmit={handleSubmit(handleLogin)}>
+        <fieldset className="fieldset">
+          <label className="label">Email</label>
+          <input type="email" {...register('email',{ require: true} )} className="input" placeholder="Email" />
+          {
+            errors.email?.type === 'required' && <p className="text-red-500">Email is required</p>
+          }
+          <label className="label">Password</label>
+          <input type="password" {...register('password', {required: true, minLength: 6})} className="input" placeholder="Password" />
+          {
+            errors.password?.type === 'minLength' && <p className="text-red-500">Password must be 6 characters longer</p>
+          }
+          <div>
+            <a className="link link-hover">Forgot password?</a>
+          </div>
+          <button className="btn btn-neutral mt-4">Login</button>
+        </fieldset>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
