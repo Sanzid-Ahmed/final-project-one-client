1f995a6a0f6d7e007d61720a76057c2e3a4e4fe5
Sanzid Ahmed <sanzidahmed313@gmail.com>
2025-11-25 10:23:45 +0600

    added Authprovider and AuthContext

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,8 @@ import { router } from './routes/router.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Authprovider>
+      <RouterProvider router={router} />
+    </Authprovider>
   </StrictMode>,
 )
--- src/pages/Auth/Register/Register.jsx
+++ src/pages/Auth/Register/Register.jsx
@@ -21,10 +21,13 @@ const Register = () => {
 
 
           <label className="label">Password</label>
-          <input type="password" {...register('password', {required:true, minLength: 6})}className="input" placeholder="Password" />
+          <input type="password" {...register('password', {required:true, minLength: 6, pattern: /^[A-Za-z]+$/i})}className="input" placeholder="Password" />
           {errors.password?.type==='required' && <p className="text-red-500">Password is required.</p>}
           {
             errors.password?.type==='minLength' && <p className="text-red-500">Password must be 6 characters or longer</p>
+          }
+          {
+            errors.password?.type==='pattern' && <p className="text-red-500">password must have </p>
           }
           <div>
             <a className="link link-hover">Forgot password?</a>
