c8867a1a2afc9bdb72625ccc982e1ebf243e5f4b
Sanzid Ahmed <sanzidahmed313@gmail.com>
2025-11-25 13:41:41 +0600

    added logout property

--- src/contexts/AuthContext/Authprovider.jsx
+++ src/contexts/AuthContext/Authprovider.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { AuthContext } from './AuthContext';
-import { createUserWithEmailAndPassword, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
+import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth';
 import { auth } from '../../firebase/firebase.init';
 
 
@@ -26,9 +26,20 @@ const Authprovider = ({children}) => {
         return signInWithPopup(auth, googleProvider);
     }
 
+    const logOut = () => {
+        setLoading(true);
+        return signOut(auth);
+    }
+
 
     useEffect(() => {
-        
+        const unSubscribe = onAuthStateChanged(auth, (currentUser)=> {
+            setUser(currentUser);
+            setLoading(false);
+        })
+        return () => {
+            unSubscribe();
+        }
     }, []);
 
     const authInfo = {
@@ -36,7 +47,8 @@ const Authprovider = ({children}) => {
         loading,
         registerUser, 
         signInUser,
-        signInGoogle
+        signInGoogle,
+        logOut
     }
 
 
