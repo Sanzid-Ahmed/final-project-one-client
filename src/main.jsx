8cc47c646707aab1eeb0e7a29970b409d3323b11
Sanzid Ahmed <sanzidahmed313@gmail.com>
2025-12-03 22:22:08 +0600

    added my percels section

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,18 @@ import './index.css'
 import App from './App.jsx'
 import { router } from './routes/router.jsx';
 import Authprovider from './contexts/AuthContext/Authprovider.jsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+
+const queryClient = new QueryClient();
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Authprovider>
-      <RouterProvider router={router} />
-    </Authprovider>
+    <QueryClientProvider client={queryClient}>
+      <Authprovider>
+        <RouterProvider router={router} />
+      </Authprovider>
+    </QueryClientProvider>
   </StrictMode>,
 )
